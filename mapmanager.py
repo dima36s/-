28d36dd51dabd5167703d4bcf0af29b4ce0363a7
Dmitry Samoylov <76055016+dima36s@users.noreply.github.com>
2024-03-12 19:30:05 +0300

    Update mapmanager.py

--- mapmanager.py
+++ mapmanager.py
@@ -34,6 +34,11 @@ class Mapmanager():
        self.block.setPos(position)
        self.color = self.getColor(int(position[2]))
        self.block.setColor(self.color)
+
+
+       self.block.setTag("at", str(position))
+
+
        self.block.reparentTo(self.land)
 
 
@@ -57,3 +62,44 @@ class Mapmanager():
                    x += 1
                y += 1
        return x,y
+  
+   def findBlocks(self, pos):
+       return self.land.findAllMatches("=at=" + str(pos))
+
+
+   def isEmpty(self, pos):
+       blocks = self.findBlocks(pos)
+       if blocks:
+           return False
+       else:
+           return True
+
+
+   def findHighestEmpty(self, pos):
+       x, y, z = pos
+       z = 1
+       while not self.isEmpty((x, y, z)):
+           z += 1
+       return (x, y, z)
+
+
+   def buildBlock(self, pos):
+       """Ставим блок с учётом гравитации: """
+       x, y, z = pos
+       new = self.findHighestEmpty(pos)
+       if new[2] <= z + 1:
+           self.addBlock(new)
+
+
+   def delBlock(self, position):
+       """удаляет блоки в указанной позиции """
+       blocks = self.findBlocks(position)
+       for block in blocks:
+           block.removeNode()
+
+
+   def delBlockFrom(self, position):
+       x, y, z = self.findHighestEmpty(position)
+       pos = x, y, z - 1
+       for block in self.findBlocks(pos):
+               block.removeNode()
